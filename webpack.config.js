72a175d707f23fe96d735e4f194c6f1f9235ac27
Prateek Joshi <prateekjoshi2013@gmail.com>
2022-10-02 21:36:23 -0700

    configures devServer for hot reloading and html-webpack-plugin for serving also reverted the manual injection of index.bundle.js in index.html this change breaks our references bcause in the generated bundle references for images and js are still pointing to source paths we need to configure loaders for assets in the next step for them to be injected and maintain references in dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: {
@@ -10,6 +11,10 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         clean: true,
     },
+    // dev server will serve the files from this folder
+    devServer: {
+        static: "./dist"
+    },
     module: {
         rules: [
             {
@@ -18,8 +23,26 @@ module.exports = {
             },
             {
                 test: /\.s[ca]ss$/,
-                use: ['style-loader', 'css-loader','sass-loader'],
+                use: ['style-loader', 'css-loader', 'sass-loader'],
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            // same as entrypoints in string
+            chunks: ["index"],
+            // to generate html file separately for these 
+            // two different pages we need to define the filename 
+            filename: "index.html",
+
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/pages/courses.html",
+            // to generate html file separately for these 
+            // two different pages we need to define the filename 
+            chunks: ["courses"],
+            filename: "courses.html",
+        })
+    ]
 }
