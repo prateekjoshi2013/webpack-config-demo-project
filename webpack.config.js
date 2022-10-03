245b8f5537352db5253ced4598ad77edec375311
Prateek Joshi <prateekjoshi2013@gmail.com>
2022-10-02 22:21:36 -0700

    configured webpack image asset loader  ,fixed image resources in index.html to newly created asset folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     entry: {
@@ -47,6 +48,23 @@ module.exports = {
             // two different pages we need to define the filename 
             chunks: ["courses"],
             filename: "courses.html",
+        }),
+        new CopyWebpackPlugin({
+            
+            patterns:[
+                {
+                    // what are the files to be copied in regex create new
+                    //  folders in src to handle different types of assets
+                    from: path.resolve(__dirname,"src/assets/images/*"),
+                    // to which root folder we need to copy these files to 
+                    // it creates the rest of the folders on the path by default
+                    to: path.resolve(__dirname,"dist"),
+                    // context is used to determine from which root folder we need to
+                    // copying if we dont specify src it will create folders starting from src
+                    // treating the outermost folder as root
+                    context: "src",
+                }
+            ]
         })
     ]
 }
