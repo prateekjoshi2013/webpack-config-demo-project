f087ac5563b6b74b29cf0984b1568d1a48e6b6fc
Prateek Joshi <prateekjoshi2013@gmail.com>
2022-10-03 21:02:04 -0700

    starting tree shaking config

---
+++ src/libs/showDate.js
@@ -0,0 +1,6 @@
+export function dummyFunction() {
+    console.log('dummy function');
+}
+
+// this is a dummy function which is not used anywhere
+//  and can be removed through tree shaking with webpack 
